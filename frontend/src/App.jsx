71b266c92d71643c8fbacd19e30ac5fa7a71d2d6
Dellen17 <gabrielkatara15@gmail.com>
2025-09-17 15:49:23 +0200

    Added environemt variables for dev and prod environments.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     setLoading(true);
     
     try {
-      const response = await fetch('http://localhost:5000/check_password', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/check_password`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
